48bd2738ffbd44dede973bf2729fea23284ceb0c
Sascha Scatá <sascha.scata@gmail.com>
2020-04-18 20:57:59 +0200

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t saschascata/multi-client:latest -t saschascata/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t saschascata/multi-server:latest -t saschascata/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t saschascata/multi-worker:latest -t saschascata/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push saschascata/multi-client:latest
+docker push saschascata/multi-server:latest
+docker push saschascata/multi-worker:latest
+
+docker push saschascata/multi-client:$SHA
+docker push saschascata/multi-server:$SHA
+docker push saschascata/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=saschascata/multi-server:docker push saschascata/multi-server:$SHA
+kubectl set image deployments/client-deployment client=saschascata/multi-client:docker push saschascata/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=saschascata/multi-worker:docker push saschascata/multi-worker:$SHA
