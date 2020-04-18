89c125b311766cfe81a0cea7193d1a14c70ed05b
Sascha Scatá <sascha.scata@gmail.com>
2020-04-18 21:14:19 +0200

    Added deployment scripts #2

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t saschascata/multi-client:latest -t saschascata/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t saschascata/multi-server:latest -t saschascata/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t saschascata/multi-worker:latest -t saschascata/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push saschascata/multi-client:latest
 docker push saschascata/multi-server:latest
 docker push saschascata/multi-worker:latest
